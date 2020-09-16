4fdf4ab499be52ede5db2b10886a2ad644e3c198
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-09-17 01:19:03 +0900

    [object & property & method]

--- Basic_JS_object/index.js
+++ Basic_JS_object/index.js
@@ -15,3 +15,12 @@ let youtuber = {
     plan() { },
     uploadVideo() { }
 };
+
+
+// // オブジェクトのアクセスの仕方。
+// // nameのしまぶーを取り出したい場合は、、以下のようにする。
+// youtuber.list.business.youtuber[0].name
+
+// // 他も同様に
+// youtuber.plan();
+// youtuber.list.entertainment.makeSmile();
